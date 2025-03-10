fcf7ec7f1337e7bc9de821989053c2b07901f3ac
“zhengtu111“ <1170520209@qq.com>
2025-03-10 14:10:00 +0800

    again test

---
+++ build/CMakeFiles/MyProject.dir/src/swap.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyProject.dir/src/swap.cpp.obj: \
+ F:\zhengtu\Thu\04PhD\git\test2\src\swap.cpp \
+ F:/zhengtu/Thu/04PhD/git/test2/include/swap.h
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//main.cpp
+#include<iostream>
+#include "swap.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+   int v1 = 100;
+   int v2 = 200;
+   cout << "Before:" << v1 << " " << v2 << endl;
+   swap(v1,v2);
+   cout << "After:" << v1 << ' ' << v2 << endl;
+   cout << "cmake调试\n";
+   cout << "按任意键继续..." << endl;
+   system("pause");
+   return 0;
+}
+
+// int main() {
+//    std::cout << "Hello, CMake!" << std::endl;
+//    system("pause");
+//    return 0;
+// }
+ 
